46d5d04ebf8f53573f8c5ac54506ce7512ec3b2e
RPI3B+ Server <wowluke@naver.com>
2021-03-06 17:29:18 +0900

    Merged to single .cpp file

---
+++ simple.cpp
@@ -0,0 +1,90 @@
+#include "simple.h"
+
+/* input pin */
+Ipin::Ipin(int p): pin(p) {
+    pinMode(pin, INPUT);
+}
+
+int Ipin::read() const {
+    return digitalRead(pin);
+}
+
+int Ipin::aread() const {
+    return analogRead(pin);
+}
+
+/* output pin */
+Opin::Opin(int p): pin(p) {
+    pinMode(pin, OUTPUT);
+}
+
+void Opin::on() const {
+    digitalWrite(pin, HIGH);
+}
+
+void Opin::on(int i) const {
+    analogWrite(pin, i);
+}
+
+void Opin::off() const {
+    digitalWrite(pin, LOW);
+}
+
+void Opin::pulse(int t) const {
+    on();
+    delay(t);
+    off();
+}
+
+/* ultrasonic sensor */
+Sonar::Sonar(int t, int e):
+    trig(t), echo(e) {;}
+
+unsigned long Sonar::measure_raw() const {
+    trig.on();
+    delayMicroseconds(t);
+    trig.off();
+    delay = pulseIn(echo.pin, HIGH); 
+    return delay;
+}
+
+float Sonar::measure() const {
+    float delay = measure_raw();
+    float distance = float(delay) * 340 / 10000 / 2;  
+    return distance;
+}
+
+/* RGB led */
+RGB::RGB(int r, int g, int b):
+    R(r), G(g), B(b) {;}
+
+void RGB::on() const {
+    R.on();
+    G.on();
+    B.on();
+}
+
+void RGB::on(int r, int g, int b) const {
+    R.on(r);
+    G.on(g);
+    B.on(b);
+}
+
+void RGB::off() const {
+    R.off();
+    G.off();
+    B.off();
+}
+
+void RGB::pulse(int t) const {
+    on();
+    delay(t);
+    off();
+}
+
+void RGB::pulse(int r, int g, int b, int t) const {
+    on(r, g, b);
+    delay(t);
+    off();
+}
+
